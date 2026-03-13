3ee0e5ecca44e7c83eda03069c14eb2d32743e3b
Jaylan <1922637728@qq.com>
2026-03-13 22:02:43 +0800

    IO多路复用之select,多线程技术实现

--- client.c
+++ client.c
@@ -1,48 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
+#include <unistd.h>
 #include <arpa/inet.h>
 
 int main()
 {
-    // 1. 创建用于通信的套接字
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if(fd == -1)
+    if (fd == -1)
     {
         perror("socket");
         exit(0);
     }
 
-    // 2. 连接服务器
     struct sockaddr_in addr;
-    addr.sin_family = AF_INET;     // ipv4
-    addr.sin_port = htons(9999);   // 服务器监听的端口, 字节序应该是网络字节序
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(9999);
     inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr.s_addr);
-    int ret = connect(fd, (struct sockaddr*)&addr, sizeof(addr));
-    if(ret == -1)
+    int ret = connect(fd, (struct sockaddr *)&addr, sizeof(addr));
+    if (ret == -1)
     {
         perror("connect");
         exit(0);
     }
 
+    int num = 0;
     // 通信
-    while(1)
+    while (1)
     {
-        // 读数据
-        char recvBuf[1024];
-        // 写数据
-        // sprintf(recvBuf, "data: %d\n", i++);
-        fgets(recvBuf, sizeof(recvBuf), stdin);
-        write(fd, recvBuf, strlen(recvBuf)+1);
-        // 如果客户端没有发送数据, 默认阻塞
-        read(fd, recvBuf, sizeof(recvBuf));
-        printf("recv buf: %s\n", recvBuf);
+        char buf[1024];
+        //从终端读一个字符
+        //fgets(buf, sizeof(buf), stdin);
+
+        sprintf(buf,"hello,world,%d\n...",num++);
+        //发数据
+        write(fd, buf, strlen(buf));
+
+        //接受
+        //阻塞等待
+        int len = read(fd,buf,sizeof(buf));
+        if(len == -1) {
+            perror("read error");
+            exit(1);
+        }
+        printf("read buf = %s\n", buf);
+        //每隔一秒发送一次
         sleep(1);
     }
-
-    // 释放资源
-    close(fd); 
-
+    close(fd);
     return 0;
 }
--- server.c
+++ server.c
@@ -1,96 +1,138 @@
-#include <sys/select.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <sys/select.h>
+#include <ctype.h>
+#include <pthread.h>
+
+//互斥锁 给共享数据加锁：maxfd,rdset 在函数中都是传地址的使用的是同一块内存； 
+pthread_mutex_t mutex;
+
+typedef struct fdinfo
+{
+    int fd;
+    int *maxfd;
+    fd_set *rdset;
+} FDInfo;
+
+void *acceptConn(void *arg)
+{
+    printf("子线程ID： %ld\n",pthread_self());
+    FDInfo *info = (FDInfo *)arg;
+    int cfd = accept(info->fd, NULL, NULL);
+    pthread_mutex_lock(&mutex);
+    FD_SET(cfd, info->rdset);
+    *info->maxfd = cfd > *info->maxfd ? cfd : *info->maxfd;
+    pthread_mutex_unlock(&mutex);
+
+    free(info);
+}
+
+// 通信函数
+void *communication(void *arg)
+{
+    printf("子线程ID： %ld\n",pthread_self());
+    FDInfo* info = (FDInfo *)arg;
+    char buf[1024] = {0};
+    int len = recv(info->fd, buf, sizeof(buf), 0);
+    if (len == 0)
+    {
+        printf("客户端关闭连接...");
+        pthread_mutex_lock(&mutex);
+        FD_CLR(info->fd, info->rdset);
+        pthread_mutex_unlock(&mutex);
+        close(info->fd);
+        free(info);
+        return NULL;
+    }
+    else if (len == -1)
+    {
+        perror("recv");
+        free(info);
+        return NULL;
+    }
+    printf("read buf = %s\n", buf);
+    for (int i = 0; i < len; i++)
+    {
+        // 小写转为大写
+        buf[i] = toupper(buf[i]);
+    }
+    printf("after buf = %s\n", buf);
+
+    // 大写发给客户端
+    int ret = send(info->fd, buf, strlen(buf) + 1, 0);
+    if (ret == -1)
+    {
+        perror("send error");
+        exit(1);
+    }
+    free(info);
+    return NULL;
+}
 
 int main()
 {
-    // 1. 创建监听的fd
+    pthread_mutex_init(&mutex,NULL);
+
+    // 1.创建用于监听的套接字
     int lfd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // 2. 绑定
+    // 2.绑定 IP 端口
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(9999);
     addr.sin_addr.s_addr = INADDR_ANY;
-    bind(lfd, (struct sockaddr*)&addr, sizeof(addr));
+    bind(lfd, (struct sockaddr *)&addr, sizeof(addr));
 
-    // 3. 设置监听
+    // 3.监听
     listen(lfd, 128);
 
-    // 将监听的fd的状态检测委托给内核检测
-    int maxfd = lfd;
-    // 初始化检测的读集合
+    // 初始化一个读集合
+    //主线程中，maxfd rdset在创建时，子线程还没有被创建出来，所以不用加锁
+    int maxfd = lfd; // 定义一个变量，用于记录当前最大的文件描述符
     fd_set rdset;
     fd_set rdtemp;
-    // 清零
     FD_ZERO(&rdset);
-    // 将监听的lfd设置到检测的读集合中
     FD_SET(lfd, &rdset);
-    // 通过select委托内核检测读集合中的文件描述符状态, 检测read缓冲区有没有数据
-    // 如果有数据, select解除阻塞返回
-    // 应该让内核持续检测
-    while(1)
-    {
-        // 默认阻塞
-        // rdset 中是委托内核检测的所有的文件描述符
-        rdtemp = rdset;
-        int num = select(maxfd+1, &rdtemp, NULL, NULL, NULL);
-        // rdset中的数据被内核改写了, 只保留了发生变化的文件描述的标志位上的1, 没变化的改为0
-        // 只要rdset中的fd对应的标志位为1 -> 缓冲区有数据了
-        // 判断
-        // 有没有新连接
-        if(FD_ISSET(lfd, &rdtemp))
-        {
-            // 接受连接请求, 这个调用不阻塞
-            struct sockaddr_in cliaddr;
-            int cliLen = sizeof(cliaddr);
-            int cfd = accept(lfd, (struct sockaddr*)&cliaddr, &cliLen);
 
-            // 得到了有效的文件描述符
-            // 通信的文件描述符添加到读集合
-            // 在下一轮select检测的时候, 就能得到缓冲区的状态
-            FD_SET(cfd, &rdset);
-            // 重置最大的文件描述符
-            maxfd = cfd > maxfd ? cfd : maxfd;
+    while (1)
+    {
+        pthread_mutex_lock(&mutex);
+        rdtemp = rdset;
+        pthread_mutex_unlock(&mutex);
+        int ret = select(maxfd + 1, &rdtemp, NULL, NULL, NULL);
+        // 判断是否为监听的fd
+        if (FD_ISSET(lfd, &rdtemp))
+        {
+            // 接收客户端的连接 //需要多线程，创建子线程,传入各个线程的fd和maxfd和rdset
+            pthread_t tid;
+            FDInfo *info = (FDInfo *)malloc(sizeof(FDInfo));
+            info->fd = lfd;
+            info->maxfd = &maxfd;
+            info->rdset = &rdset;
+            pthread_create(&tid, NULL, acceptConn, info);
+            // 将主线程和子线程进行线程分离
+            pthread_detach(tid);
         }
 
-        // 没有新连接, 通信
-        for(int i=0; i<maxfd+1; ++i)
+        for (int i = 0; i < maxfd + 1; i++)
         {
-			// 判断从监听的文件描述符之后到maxfd这个范围内的文件描述符是否读缓冲区有数据
-            if(i != lfd && FD_ISSET(i, &rdtemp))
+            if (i != lfd && FD_ISSET(i, &rdtemp))
             {
                 // 接收数据
-                char buf[10] = {0};
-                // 一次只能接收10个字节, 客户端一次发送100个字节
-                // 一次是接收不完的, 文件描述符对应的读缓冲区中还有数据
-                // 下一轮select检测的时候, 内核还会标记这个文件描述符缓冲区有数据 -> 再读一次
-                // 	循环会一直持续, 知道缓冲区数据被读完位置
-                int len = read(i, buf, sizeof(buf));
-                if(len == 0)
-                {
-                    printf("客户端关闭了连接...\n");
-                    // 将检测的文件描述符从读集合中删除
-                    FD_CLR(i, &rdset);
-                    close(i);
-                }
-                else if(len > 0)
-                {
-                    // 收到了数据
-                    // 发送数据
-                    write(i, buf, strlen(buf)+1);
-                }
-                else
-                {
-                    // 异常
-                    perror("read");
-                }
+                pthread_t tid;
+                FDInfo *info = (FDInfo *)malloc(sizeof(FDInfo));
+                info->fd = i;
+                info->rdset = &rdset;
+                pthread_create(&tid, NULL, communication, info);
+                // 将主线程和子线程进行线程分离
+                pthread_detach(tid);
             }
         }
     }
-
+    close(lfd);
+    pthread_mutex_destroy(&mutex);
     return 0;
 }
